4f41929c48ead57a46a8dea52124b3eb57ac111e
Sofianel5 <sofianef1110@gmail.com>
2025-05-19 21:32:34 -0700

    fix: passing tests yay

--- tests/quantum-markets.ts
+++ tests/quantum-markets.ts
@@ -1,7 +1,7 @@
 import * as anchor from "@coral-xyz/anchor";
 import { QuantumMarkets } from "../target/types/quantum_markets";
 import { PublicKey, Keypair, SystemProgram } from "@solana/web3.js";
-import { createMint, getAssociatedTokenAddress, mintTo, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { createMint, ASSOCIATED_TOKEN_PROGRAM_ID, mintTo, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { QuantumMarkets, IDL } from "../target/types/quantum_markets"
 import { assert } from "chai";
 
@@ -16,6 +16,7 @@ describe("quantum-markets", () => {
   const payer = provider.wallet.payer as Keypair;
   let rewardMint: PublicKey;
   let globalPda: PublicKey, globalBump: number;
+  let marketPda: PublicKey, marketBump: number;
 
   // derive the “global” state PDA
   before(async () => {
@@ -39,5 +40,56 @@ describe("quantum-markets", () => {
     );
     assert.ok(rewardMint);
   });
+
+  it("Initializes global state", async () => {
+    await program.methods
+      .initializeGlobal()
+      .accounts({
+        payer: payer.publicKey,
+      })
+      .rpc();
+
+    const g = await program.account.globalState.fetch(globalPda);
+    assert.equal(g.nextId.toNumber(), 0);
+  });
+
+  it("Creates the first market", async () => {
+    // derive the market PDA (uses global.nextMarketId == 0)
+    [marketPda, marketBump] = PublicKey.findProgramAddressSync(
+      [Buffer.from("market"), Buffer.from(Uint8Array.of(0,0,0,0,0,0,0,0))],
+      program.programId
+    )
+
+
+    await program.methods
+      .createMarket(
+        /* minDeposit */ new anchor.BN(1000),
+        /* strikePrice */ new anchor.BN(42),
+        /* title */ "My First Market"
+      )
+      .accounts({
+        global: globalPda,
+        payer: payer.publicKey,
+        rewardMint,
+        resolver: payer.publicKey,   // for now just use yourself
+        market: marketPda,
+        systemProgram: SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+      })
+      .rpc();
+
+    // fetch and assert
+    const m = await program.account.marketConfig.fetch(marketPda);
+    assert.equal(m.id.toNumber(), 0);
+    assert.equal(m.minDeposit.toNumber(), 1000);
+    assert.equal(m.strikePrice.toNumber(), 42);
+    assert.equal(m.title, "My First Market");
+    assert.deepEqual(m.status, { open: {} });
+    assert.ok(m.marketToken.equals(rewardMint));
+    assert.ok(m.resolver.equals(payer.publicKey));
+  });
+
+
 
 })
